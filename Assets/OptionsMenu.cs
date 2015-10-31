545e4487a2f0c52e5b3f38a6547928fdec48414c
TheMSB <Martijn.bruning@gmail.com>
2015-10-31 09:32:21 +0100

    added a working options menu, volume options still have to be linked through game controller.

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsMenu : Menu {
+
+	public UserProfile profile;
+	public InputField nameField;
+	public Slider musicValue;
+	public Slider effectValue;
+
+	// Use this for initialization
+	void Start () {
+		if (profile.GetName () != null) {
+			nameField.text = profile.GetName();
+		}
+		if (profile.GetMusicVolume() != null) {
+			musicValue.value = profile.GetMusicVolume();
+		}
+		if (profile.GetEffectsVolume() != null) {
+			effectValue.value = profile.GetEffectsVolume();
+		}
+	}
+}
---
+++ Assets/UserProfile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * The UserProfile class is responsible for handeling all scene and
+ * session persistent data.
+ * 
+ * This includes but is not limited to: player names, volume preferences
+ * and scores.
+ */
+public class UserProfile : MonoBehaviour {
+
+	/*
+	 * Clears a specific key from the player prefs, 
+	 * if the key is null then the entire PlayerPrefs will be cleared.
+	 */ 
+	public void Clear (string key) {
+		if (key == null) {
+			PlayerPrefs.DeleteAll ();
+		} else {
+			if (PlayerPrefs.HasKey(key)) {
+				PlayerPrefs.DeleteKey(key);
+			}
+		} 
+	}
+
+	public void SetName(string name) {
+		PlayerPrefs.SetString ("name", name);
+	}
+
+	public void SetEffectsVolume(float value) {
+		PlayerPrefs.SetFloat ("effectsVolume", value);
+	}
+
+	public void SetMusicVolume(float value) {
+		PlayerPrefs.SetFloat ("musicVolume", value);
+	}
+
+	public string GetName () {
+		return PlayerPrefs.GetString ("name");
+	}
+
+	public float GetEffectsVolume () {
+		return PlayerPrefs.GetFloat ("effectsVolume");
+	}
+
+	public float GetMusicVolume () {
+		return PlayerPrefs.GetFloat ("musicVolume");
+	}
+}
