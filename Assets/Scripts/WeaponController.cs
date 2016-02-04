6fd08c08af87281445cb048e815b2b7f73eb8d41
TheMSB <Martijn.bruning@gmail.com>
2016-02-04 17:21:58 +0100

    fixed strafer weapon velocity and added muzzle flash

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class WeaponController : MonoBehaviour {
 
 	public GameObject shot;
-	public float shotThrust;
-	private ParticleSystem muzzle;
 	public Transform shotSpawn;
 	public float fireRate;
 	public float startDelay;
 	public AudioSource sound;
 
+	private ParticleSystem muzzle;
+	
 	// Start firing the gun using set interval or pattern
 	void Start () {
 		sound = GetComponent<AudioSource> ();
@@ -19,12 +19,13 @@ public class WeaponController : MonoBehaviour {
 	}
 
 	void Fire() {
+		// If we have a muzzle flash made for us then use it
 		if (muzzle != null) {
 			muzzle.Emit(1);
 		}
 		GameObject shotRef = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+		// Ensure that the velocity of the shooter is added to the shot
 		shotRef.transform.GetComponent<Rigidbody> ().AddForce (this.gameObject.transform.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
-		Debug.Log (this.gameObject.name);
 		sound.Play ();
 
 	}
