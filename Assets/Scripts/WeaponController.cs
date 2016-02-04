51b7956afcb1b12d05f136faa7d8de917f09973f
TheMSB <Martijn.bruning@gmail.com>
2016-02-04 17:03:48 +0100

    Crash recovery

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class WeaponController : MonoBehaviour {
 
 	public GameObject shot;
+	public float shotThrust;
+	private ParticleSystem muzzle;
 	public Transform shotSpawn;
 	public float fireRate;
 	public float startDelay;
@@ -13,10 +15,17 @@ public class WeaponController : MonoBehaviour {
 	void Start () {
 		sound = GetComponent<AudioSource> ();
 		InvokeRepeating ("Fire", startDelay, fireRate);
+		muzzle = shotSpawn.GetComponent<ParticleSystem> ();
 	}
 
 	void Fire() {
-		Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		if (muzzle != null) {
+			muzzle.Emit(1);
+		}
+		GameObject shotRef = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+		shotRef.transform.GetComponent<Rigidbody> ().AddForce (this.gameObject.transform.GetComponent<Rigidbody>().velocity, ForceMode.VelocityChange);
+		Debug.Log (this.gameObject.name);
 		sound.Play ();
+
 	}
 }
